bba8119dc5040c6c4c6ec13bb11b2c818060e844
Leah <leah23531@gmail.com>
2025-03-26 16:59:15 +0200

    26/03/2025

---
+++ OrderManagementSystem/OrderManagementSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using Bl;
+using Bl.Api;
+using Bl.Services;
+using Bl.Models;
+
+
+namespace OrderManagementSystem
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            IBL bl = new BlManager();
+
+
+            var list = await bl.Order.GetAll();
+            foreach (BLOrder o in list)
+            {
+                Console.WriteLine("Order ID number:" + o.IdOrder + " "
+                                  + o.DateOrder.ToString("dd/MM/yyyy"));
+            }
+            var list1 = await bl.Order.GetAll();
+            await BLOrderService.GenerateOrdersPdf(list);
+        }
+    }
+}
+
+
+//https://github.com/esther128979/projectAI.git
