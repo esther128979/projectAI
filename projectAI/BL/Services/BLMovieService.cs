af6dc4b871a1da966118ec7e63c0b5f119dccbbc
קמפנר אסתר <214991887@main.local>
2025-05-19 09:46:59 +0300

    Add BLManeger UpDate

--- projectAI/BL/Services/BLMovieService.cs
+++ projectAI/BL/Services/BLMovieService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using BL.Api;
+using BL.Models;
+using Dal.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,36 @@ using System.Threading.Tasks;
 
 namespace BL.Services
 {
-    internal class BLMovieService
+    public class BLMovieService : IBLMovies
     {
+
+        IDal dal;
+        IMapper mapper;
+        public BLMovieService(IDal d)
+        {
+            dal = d;
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+             mapper = config.CreateMapper();
+        }
+
+        public Task<List<BLMovie>> GetAll()
+        {
+            List<BLMovie> list = new List<BLMovie>();
+
+            var dallist = dal.Movie.GetAll().Result;
+
+            dallist.ForEach(l=>list.Add( mapper.Map<BLMovie >(l)));
+
+            return list;
+        }
+
+        public Task<BLMovie> GetMovieByCategory()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
